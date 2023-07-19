2f082632839b04c5e5d1dcd72bf1248426278eb4
zeeshan <mirzazeeshanmusatafa1@yahoo.com>
2023-07-19 16:36:31 +0500

    new router file made and all post routes called

---
+++ server/routes/posts.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {createPosts, getPosts} from "../controllers/posts.js";
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.get('/', createPosts);
+
+export default router
