1f674fd20058bd3600b8d84e1a67c7c227efa86f
zeeshan <mirzazeeshanmusatafa1@yahoo.com>
2023-07-20 12:26:16 +0500

    redux configure

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {applyMiddleware, compose, createStore} from "redux";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
