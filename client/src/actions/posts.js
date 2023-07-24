ca7c6c53f33ddbfc38960d1a94fdb3f16f7cbf09
zeeshan <mirzazeeshanmusatafa1@yahoo.com>
2023-07-24 13:29:52 +0500

    api call function added to create post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -3,6 +3,18 @@ import * as api from '../api'
 export const getPosts = () => async (dispatch) => {
   try {
     const {data} = await api.fetchPost()
+
+    dispatch({type: 'FETCH_ALL', payload: data})
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+export const createPosts = (post) => async (dispatch) => {
+  try {
+    const {data} = await api.createPost(post)
+
+    dispatch({type: 'CREATE', payload: data})
   } catch (error) {
     console.log(error.message)
   }
