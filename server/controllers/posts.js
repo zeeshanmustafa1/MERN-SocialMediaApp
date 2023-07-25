a07efe6b856ff4dda0bbee43a9f3cab3ddc2f51f
zeeshan <mirzazeeshanmusatafa1@yahoo.com>
2023-07-25 16:59:27 +0500

    new like route added and controller added to update the like

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -35,9 +35,21 @@ export const updatePost = async (req, res) => {
 }
 
 export const deletePost = async (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
   await PostMessage.findByIdAndRemove(id);
-  res.json({ message: "Post deleted successfully." });
+  res.json({message: "Post deleted successfully."});
 }
+
+export const likePost = async (req, res) => {
+  const {id} = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    {likeCount: post.likeCount + 1}, {new: true}
+  );
+  res.json(updatedPost);
+}
+
