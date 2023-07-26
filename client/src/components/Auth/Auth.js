67022fd657b69804b08d71c32daa6c362a168ffc
zeeshan <mirzazeeshanmusatafa1@yahoo.com>
2023-07-26 18:52:12 +0500

    navbar and home component made

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <h1>
+      Auth
+    </h1>
+  );
+};
+
+export default Auth;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import {AppBar, Avatar, Toolbar, Typography} from "@material-ui/core";
+import memories from "../../images/memories.png";
+import useStyle from './style';
+import {Button} from "@material-ui/core/";
+
+const Navbar = () => {
+  const classes = useStyle();
+  const user = null;
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60"/>
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.propile}>
+            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button variant="contained" className={classes.logout} color="secondary">
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
